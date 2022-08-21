69f1f14815cfa7157f8aba79172f2173c5b5a7e8
Akhilesh <theakhileshjoshi@gmail.com>
2022-08-21 23:15:05 +0530

    Registration form update

--- src/isItLive/registration.java
+++ src/isItLive/registration.java
@@ -3,10 +3,20 @@ package isItLive;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
 
 public class registration {
 
 	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
 
 	/**
 	 * Launch the application.
@@ -36,8 +46,69 @@ public class registration {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setFont(new Font("Dialog", Font.PLAIN, 30));
+		frame.setBounds(0,0, 1280, 720);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("isItLive");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 36));
+		lblNewLabel.setBounds(632, 39, 188, 39);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Name");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lblNewLabel_1.setBounds(463, 127, 133, 28);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		textField = new JTextField();
+		textField.setBounds(687, 127, 255, 28);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Submit");
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		btnNewButton.setBounds(632, 560, 133, 39);
+		frame.getContentPane().add(btnNewButton);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("E-mail");
+		lblNewLabel_1_1.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lblNewLabel_1_1.setBounds(463, 194, 133, 28);
+		frame.getContentPane().add(lblNewLabel_1_1);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(687, 194, 255, 28);
+		frame.getContentPane().add(textField_1);
+		
+		JLabel lblNewLabel_1_2 = new JLabel("Website URL");
+		lblNewLabel_1_2.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lblNewLabel_1_2.setBounds(463, 261, 188, 28);
+		frame.getContentPane().add(lblNewLabel_1_2);
+		
+		JLabel lblNewLabel_1_1_1 = new JLabel("Number of Hours");
+		lblNewLabel_1_1_1.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lblNewLabel_1_1_1.setBounds(463, 328, 172, 28);
+		frame.getContentPane().add(lblNewLabel_1_1_1);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(687, 261, 255, 28);
+		frame.getContentPane().add(textField_2);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		textField_3.setBounds(687, 328, 125, 28);
+		frame.getContentPane().add(textField_3);
+		
+		JLabel lblNewLabel_1_2_1 = new JLabel("Message for reminder");
+		lblNewLabel_1_2_1.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lblNewLabel_1_2_1.setBounds(463, 395, 211, 28);
+		frame.getContentPane().add(lblNewLabel_1_2_1);
+		
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		textField_4.setBounds(687, 395, 255, 124);
+		frame.getContentPane().add(textField_4);
 	}
-
 }
